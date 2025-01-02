f61b52d49ad2a47783cb3d7865192311e162ec7c
Gregor <danylo.polikanov@ut.ee>
2025-01-02 20:57:02 +0200

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Esti_bus_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
